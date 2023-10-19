f388af547aae1bbb80cf9635c9ef405b9c936779
Osumah John Terry <osumahterry@gmail.com>
2023-10-19 16:12:38 +0100

    setup

--- build/server.js
+++ build/server.js
@@ -6,7 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const config_1 = __importDefault(require("config"));
 const connect_1 = __importDefault(require("./db/connect"));
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
 (0, connect_1.default)(config_1.default.get("database"));
 app.listen(config_1.default.get("PORT"), () => {
     console.log("server started on port", config_1.default.get("PORT"));
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import config from "config";
 import connectDB from "./db/connect";
 import express from "express";
+import bodyParser from "body-parser";
 import { Request, Response, NextFunction } from "express";
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 connectDB(config.get("database"));
 app.listen(config.get("PORT"), () => {
