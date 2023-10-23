4fca17b30326539c93e21c3e5ae385fff8772851
Osumah John Terry <osumahterry@gmail.com>
2023-10-23 19:28:28 +0100

    helpers controllers

--- src/helpers/user.ts
+++ src/helpers/user.ts
@@ -1,6 +1,39 @@
 import iUser from "../types/user";
+import User from "../models/user";
+import createError from "http-errors";
 
 export const processUpdate = async (
   userId: String,
   userModelValidation: iUser
-) => {};
+) => {
+  try {
+    const updateUser = await User.findOneAndUpdate(
+      { _id: userId },
+      {
+        name: userModelValidation.name,
+        username: userModelValidation.username,
+      },
+      {
+        new: true,
+      }
+    );
+
+    return updateUser;
+  } catch (error: any) {
+    console.log(error);
+  }
+};
+
+export const addUser = (userModelValidation: iUser) => {
+  try {
+    const user = User.create({
+      username: userModelValidation.username,
+      name: userModelValidation.name,
+      email: userModelValidation.email,
+      password: userModelValidation.password,
+    });
+    return user;
+  } catch (error: any) {
+    throw createError.BadRequest("Bad Request");
+  }
+};
