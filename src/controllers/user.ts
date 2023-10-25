0707255364070c5953ba24aacaa013800f6cc2c8
Osumah John Terry <osumahterry@gmail.com>
2023-10-25 12:46:13 +0100

    controller error fix

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -30,5 +30,44 @@ export const createUser = async (
     const userModelValidation: iUser = await UserValidation.validateAsync(
       req.body
     );
-  } catch (error: any) {}
+
+    if (!userModelValidation) {
+      return next(
+        new createError.BadRequest(
+          "Operation failed, invalid details provided."
+        )
+      );
+    } else {
+      const isUserNameAvailable = await User.findOne({
+        username: userModelValidation.username,
+      });
+      if (isUserNameAvailable) {
+        res.status(400).json({
+          message: `Username ${userModelValidation.username} is not available`,
+        });
+      } else {
+        const newUser = await addUser(userModelValidation);
+        if (newUser) {
+          res.status(201).json({
+            newUser,
+          });
+        } else {
+          return next(
+            res.status(400).json({
+              message: "Invalid details provided.",
+            })
+          );
+        }
+      }
+    }
+  } catch (error: any) {
+    if (error.isJoi === true) {
+      return next(
+        res.status(400).json({
+          message: "Invalid details provided.",
+        })
+      );
+    }
+    next(error);
+  }
 };
